86063b0f3e1d449f48f988e1241106531113fd33
Viacheslav Shcherbak <vbshcherbak@luxoft.com>
2022-05-09 12:49:33 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include "montezuma.h"
+
+
+int main(int argc, char *argv[])
+{
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+    QGuiApplication app(argc, argv);
+
+    Montezuma montezuma;
+    qmlRegisterType<Montezuma>("plugins.montezuma", 1, 0, "Montezuma");
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
